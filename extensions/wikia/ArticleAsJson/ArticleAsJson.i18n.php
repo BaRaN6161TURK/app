855a1efdc6731f0bfa5a041747880621225d2060
BaRaN6161TURK <53825631+BaRaN6161TURK@users.noreply.github.com>
2019-09-08 16:50:33 +0300

    Turkish
    Modifying Turkish language.

--- extensions/wikia/ArticleAsJson/ArticleAsJson.i18n.php
+++ extensions/wikia/ArticleAsJson/ArticleAsJson.i18n.php
@@ -14,3 +14,6 @@ $messages['fr'] = [
 	'articleasjson-desc' => 'Extension qui extrait tout ce qui est possible d\'un article pour l\'envoyer à un client au format JSON',
 ];
 
+$messages['tr'] = [
+	'articleasjson-desc' => 'Bir makaleden bir müşteriye json olarak gönderebildiğimiz kadarını çıkaran uzantı',
+];
